3cee1ab2be0fe6c87ec11495a62b58a6a2996078
宋俊伯 <634052666@qq.com>
2022-11-20 15:04:03 +0800

    富文本 提交

---
+++ Assets/Scripts/UIExtension/RichTextMeshPro.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UIExtension
+{
+    public class RichTextMeshPro : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            TMP_Text tmp = GetComponent<TMP_Text>();
+            int linkIndex =TMP_TextUtilities.FindIntersectingLink(tmp,eventData.position,null);
+            if (linkIndex != -1)
+            {
+                TMP_LinkInfo linkInfo = tmp.textInfo.linkInfo[linkIndex];
+                Debug.Log(linkInfo.GetLinkID());
+                Application.OpenURL(linkInfo.GetLinkID());
+            }
+        }
+    }
+}
